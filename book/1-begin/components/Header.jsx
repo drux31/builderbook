d6ebd46025272800bf2447271d6a239d36ed2405
landryx <landry@lnts.me>
2025-07-14 00:39:41 +0200

    adding grid and toolbar to the page using material-UI

--- book/1-begin/components/Header.jsx
+++ book/1-begin/components/Header.jsx
@@ -1,10 +1,21 @@
 import Link from 'next/link';
 
+import Toolbar from '@mui/material/Toolbar';
+import Grid from '@mui/material/Grid';
+
+import { styleToolbar } from './SharedStyles';
+
 const Header = () => (
   <div>
-    <Link href="/login">
-      <span style={{ margin: '0px 20px 0px auto' }}>Log in</span>
-    </Link>
+    <Toolbar style={styleToolbar}>
+      <Grid container direction="row" justifyContent="space-around" align="center">
+        <Grid item xs={12} style={{ textAlign: 'right' }}>
+          <Link href="/login">
+            <span style={{ margin: '0px 20px 0px auto' }}>Log in</span>
+          </Link>
+        </Grid>
+      </Grid>
+    </Toolbar>
   </div>
 );
 
